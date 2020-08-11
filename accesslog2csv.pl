74989caa98421511ee86f4259c4ac22a269cd650
Rowan Wookey <admin@rwky.net>
2020-08-11 15:09:35 +0100

    Enabled use strict, use warnings. Adjusted output order so fields which may contain a , will end up at the end of the file this makes it easier to pass to tools like `sort`. Removed quotes around response times since they're always floats and this improves usage with tools like `sort`

--- accesslog2csv.pl
+++ accesslog2csv.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
  
 #
 # @file
@@ -26,48 +28,50 @@
 #   Copyright 2020 Rowan Wookey <admin@rwky.net>
 #
  
-if ("$ARGV[0]" =~ /^-h|--help$/) {
+if (defined $ARGV[0] && "$ARGV[0]" =~ /^-h|--help$/) {
   print "Usage: $0 access_log_file > csv_output_file.csv\n";
   print "   Or, $0 < access_log_file > csv_output_file.csv\n";
   print "   Or, $0 < access_log_file > csv_output_file.csv 2> invalid_lines.txt\n";
   exit(0);
 }
  
-%MONTHS = ( 'Jan' => '01', 'Feb' => '02', 'Mar' => '03', 'Apr' => '04', 'May' => '05', 'Jun' => '06',
+my %MONTHS = ( 'Jan' => '01', 'Feb' => '02', 'Mar' => '03', 'Apr' => '04', 'May' => '05', 'Jun' => '06',
   'Jul' => '07', 'Aug' => '08', 'Sep' => '09', 'Oct' => '10', 'Nov' => '11', 'Dec' => '12' );
  
-print STDOUT "\"Host\",\"Date Time\",\"Method\",\"URL\",\"Response Code\",\"Bytes Sent\",\"Referer\",\"User Agent\"\,\"X-Forwarded-For\",\"Request Time\",\"Upstream Response Time\"\r\n";
-$line_no = 0;
-$invalid = 0;
+print STDOUT "\"Host\",\"Date Time\",\"Method\",\"Response Code\",\"Bytes Sent\",\"Request Time\",\"Upstream Response Time\",\"URL\",\"Referer\",\"User Agent\"\,\"X-Forwarded-For\"\r\n";
+my $line_no = 0;
+my $invalid = 0;
  
 while (<>) {
   ++$line_no;
   if (/^([\w\.:-]+) - [\w\.:-]+\s+\[(\d+)\/(\w+)\/(\d+):(\d+):(\d+):(\d+)\s?[\w:\+-]+\]\s+"(\w+)\s+(\S+)\s+HTTP\/\d\.\d"\s+(\d+)\s+([\d-]+)(?:(?:\s+"([^"]*?)"\s+")?([^"]*?)")?\s+"([\w\.:,\s-]+)"\s+(\d+\.\d+)\s+([\d\.-]+)$/) {
-    $host = $1;
-    $day = $2;
-    $month = $MONTHS{$3};
-    $year = $4;
-    $hour = $5;
-    $min = $6;
-    $sec = $7;
-    $method = $8;
-    $url = $9;
-    $code = $10;
+    my $host = $1;
+    my $day = $2;
+    my $month = $MONTHS{$3};
+    my $year = $4;
+    my $hour = $5;
+    my $min = $6;
+    my $sec = $7;
+    my $method = $8;
+    my $url = $9;
+    my $code = $10;
+    my $bytesd;
     if ($11 eq '-') {
       $bytesd = 0;
     } else {
       $bytesd = $11;
     }
-    $referer = $12;
-    $ua = $13;
-    $xforwarded = $14;
-    $time = $15;
+    my $referer = $12;
+    my $ua = $13;
+    my $xforwarded = $14;
+    my $time = $15;
+    my $upstream;
     if ($16 eq '-') {
-	$upstream = 0;
+	    $upstream = 0;
     } else {
     	$upstream = $16;
     }
-    print STDOUT "\"$host\",\"$year-$month-$day $hour:$min:$sec\",\"$method\",\"$url\",$code,$bytesd,\"$referer\",\"$ua\",\"$xforwarded\",\"$time\",\"$upstream\"\r\n";
+    print STDOUT "\"$host\",\"$year-$month-$day $hour:$min:$sec\",\"$method\",$code,$bytesd,$time,$upstream,\"$url\",\"$referer\",\"$ua\",\"$xforwarded\"\r\n";
   } else {
 	  ++$invalid;
     print STDERR "Invalid Line at $line_no: $_";
